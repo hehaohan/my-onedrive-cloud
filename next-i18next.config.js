36cf21874fc1f88d5a9d35b26067630c253f5d58
Jason He <59168084+hehaohan@users.noreply.github.com>
2026-04-21 00:07:40 +0800

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,10 +3,22 @@ const path = require('path')
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    // 覆盖了原项目所有的语言种类
-    locales:['en', 'zh', 'tw', 'ja', 'es', 'ko', 'fr', 'de', 'ru', 'tr', 'id', 'vi'],
+    // 🚨 拨乱反正：恢复原项目极其严谨的国际标准语言代号！
+    locales:[
+      'en',
+      'zh-CN',
+      'zh-TW',
+      'ja',
+      'es',
+      'ko',
+      'fr',
+      'de',
+      'ru',
+      'tr-TR',
+      'id',
+      'vi'
+    ],
   },
-  // 🚨 核心魔法：向 Vercel 发出死命令，无论在云端还是本地，必须沿着这个绝对路径把字典带上！
   localePath: typeof window === 'undefined'
     ? path.resolve('./public/locales')
     : '/locales',
