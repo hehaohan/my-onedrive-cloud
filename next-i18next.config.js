3fc40f70bf2644ed40cca3fdbbb802a886cbab69
Jason He <59168084+hehaohan@users.noreply.github.com>
2026-04-21 00:11:56 +0800

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,21 +3,7 @@ const path = require('path')
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    // 🚨 拨乱反正：恢复原项目极其严谨的国际标准语言代号！
-    locales:[
-      'en',
-      'zh-CN',
-      'zh-TW',
-      'ja',
-      'es',
-      'ko',
-      'fr',
-      'de',
-      'ru',
-      'tr-TR',
-      'id',
-      'vi'
-    ],
+    locales: ['de-DE', 'en', 'es', 'zh-CN', 'hi', 'id', 'tr-TR', 'zh-TW']
   },
   localePath: typeof window === 'undefined'
     ? path.resolve('./public/locales')
